3433ddc9b35e009fbe7e52562c4d3ae5e0c933cd
Nidula Gunawardana <nidulam@outlook.com>
2024-01-27 21:18:34 +0530

    writing to console

---
+++ Tutorial/readingFromConsole.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a value: ";
+    
+    double x;
+    double y;
+    
+    cin >> x;
+    cin >> y;
+    
+    cout << x + y;
+
+    return 0;
+}
