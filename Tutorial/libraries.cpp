e5c73c526306e9a40ddfea12a8f3b20cdfa3d0fe
Nidula Gunawardana <nidulam@outlook.com>
2024-01-27 21:25:21 +0530

    using cmath done

---
+++ Tutorial/libraries.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double result = floor(1.2);
+    cout << result << endl;
+
+    double power = pow(2, 3);
+    cout << power;
+
+    return 0;
+}
