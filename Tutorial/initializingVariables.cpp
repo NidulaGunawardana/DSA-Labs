1ce56a55bc2f8314233ec9a34d5d785205c7e894
Nidula Gunawardana <nidulam@outlook.com>
2024-01-27 21:47:22 +0530

    variables

---
+++ Tutorial/initializingVariables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double price = 99.99;
+    float intrestRate = 3.67F;
+
+    long fileSize = 90000L;
+    char letter = 'a';
+    bool isValid = false;
+
+    auto name = false;
+    auto file = 90000L;
+
+    int number {1};
+    cout << number;
+
+    return 0;
+}
